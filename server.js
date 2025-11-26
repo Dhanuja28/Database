5513e574bf07c81f9876c6079a6974ec9bd2261c
dhanuja28 <dhanujakc28@gmail.com>
2025-11-26 16:46:22 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql2');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "testconnection",
+  database: "tallydata"
+});
+
+// Test Route
+app.get("/api/ledgers", (req, res) => {
+  const query = "SELECT LedName, Parent, Billwise FROM Ledger";
+  
+  db.query(query, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("✔ API Running at http://localhost:5000");
+});
